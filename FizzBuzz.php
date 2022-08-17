1955e0e27a9b268e006e4eb6cf32abdae89c2e92
Nicolas <nicolas.dimitriadis@student.vinci.be>
2022-08-17 14:41:07 +0200

    Feature: Add testCountGiven2ShouldReturn2() test method

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -6,4 +6,11 @@ class FizzBuzz
     // tous les multiples de 5 sont remplacés par Buzz
     // tous les multiples de 3 ET 5 sont remplacés par FizzBuzz
     //1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, ....
+
+    public function count() : string{
+        $return = "";
+
+
+        return "1";
+    }
 }
--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,11 +1,35 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+require_once("FizzBuzz.php");
 
 class FizzBuzzTest extends TestCase {
+    public function testCountGiven1ShouldReturn1() : void {
+        //Arrange : Prépapration des besoins pour pouvoir exécuter la fonction testé
+        $fizzBuzz = new FizzBuzz();
 
-    public function testDemo() : void {
-        $this-> assertEquals(true, true);
+        //Act : Lance la fonction que l'on souhaite tester
+        //La variable $actual contiendra du coup le résultat (le return) de la fonction appelé
+        $actual = $fizzBuzz->count();
+
+        //Assert : vérfications des résultats de la fonction
+        $this->assertEquals("1",$actual);
     }
+
+
+    public function testCountGiven2ShouldReturn2() : void {
+        //Arrange
+        $fizzBuzz = new FizzBuzz();
+
+        //Act
+        $actual = $fizzBuzz.count();
+
+        //Assert
+        $this->assertEquals("2", $actual);
+    }
+
+//    public function testDemo() : void {
+//        $this-> assertEquals(true, true);
+//    }
 }
 

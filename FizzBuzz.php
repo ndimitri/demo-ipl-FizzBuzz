890a8618f69004011740a52a7a66d161f6f9003a
Nicolas <nicolas.dimitriadis@student.vinci.be>
2022-08-17 13:16:19 +0200

    Feature: Needed TestClasses

---
+++ FizzBuzz.php
@@ -0,0 +1,9 @@
+<?php
+
+class FizzBuzz
+{
+    // tous les multiples de 3 sont remplacés par Fizz
+    // tous les multiples de 5 sont remplacés par Buzz
+    // tous les multiples de 3 ET 5 sont remplacés par FizzBuzz
+    //1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, ....
+}
---
+++ FizzBuzzTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase {
+    /**
+     * @return void
+     */
+    public function testDemo() : void {
+        $this-> assertEquals(true, true);
+    }
+}
+

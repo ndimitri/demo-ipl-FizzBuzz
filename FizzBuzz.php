0071a77b0ada0dfc9018e80ddf657d2055875ea2
Nicolas Dimitriadis <dimitriadisnicolas@gmail.com>
2022-08-17 14:59:31 +0200

    refactor: refactor FizzBuzz method with strval() function

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -8,10 +8,8 @@ class FizzBuzz
     //1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, ....
 
     public function count($value) : string{
-        if($value === 1) return "1";
-
         if($value === 3) return "fizz";
 
-        return "2";
+        return strval($value);
     }
 }

6f566f6e1a164bd577fe0e3a4b0b7039672250e0
Nicolas Dimitriadis <dimitriadisnicolas@gmail.com>
2022-08-17 15:24:06 +0200

    refactor: add some comments

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -8,6 +8,7 @@ class FizzBuzz
     //1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, ....
 
     public function count($value) : string{
+        //Modifie some values and add some comments
         if($value % 3 === 0 && $value % 5 ===0) return "fizz buzz";
         if($value % 3 === 0) return "fizz";
         if($value % 5 === 0) return "buzz";
